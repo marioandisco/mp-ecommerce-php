20747d54ad88b80b25df4a5d34ef81e154af0780
marioandisco <72173232+marioandisco@users.noreply.github.com>
2020-10-03 22:50:59 -0300

    Update pagar.php

--- pagar.php
+++ pagar.php
@@ -1,6 +1,7 @@
 <?php
 // SDK de Mercado Pago
-require __DIR__ .  '/vendor/autoload.php';
+//require __DIR__ .  '/vendor/autoload.php';
+include_once("vendor/autoload.php");
 
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
