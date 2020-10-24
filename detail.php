7e4818138b33214f3de8f67133aa5bfa40b0f38c
marioandisco <72173232+marioandisco@users.noreply.github.com>
2020-10-24 10:18:54 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -6,7 +6,7 @@ require '/vendor/autoload.php';
 //include_once("/vendor/autoload.php");
 
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+//MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
 //$datos = array();
 // Crea un objeto de preferencia
