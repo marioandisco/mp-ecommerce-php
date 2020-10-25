29a32c21a196e7dc799cf80f730187d58ec3afc8
marioandisco <72173232+marioandisco@users.noreply.github.com>
2020-10-25 00:11:00 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,12 +1,12 @@
 <?php
 // SDK de Mercado Pago
 //require __DIR__ .  "/vendor/autoload.php";
-require '/vendor/autoload.php';
+require 'vendor/autoload.php';
 //require_once "/vendor/autoload.php";
 //include_once("/vendor/autoload.php");
 
 // Agrega credenciales
-//MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
 //$datos = array();
 // Crea un objeto de preferencia
@@ -39,6 +39,7 @@ $preference->save();
     src="https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
     crossorigin="anonymous"></script>
+    <script src="https://www.mercadopago.com/v2/security.js" view="item"></script>
     <script src="https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js"></script>
 
     <link rel="stylesheet" href="./assets/category-landing.css" media="screen, print">
